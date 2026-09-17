190aa0b0252b90c130e512b51bd54667167deb22
Taylor Grote <tgrote@midwestlabs.com>
2026-09-16 21:28:20 -0500

    Enable Personal Home Page LSP

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -10,13 +10,13 @@ return {
 		"mason-org/mason-lspconfig.nvim",
 		dependencies = { "mason-org/mason.nvim", "neovim/nvim-lspconfig" },
 		opts = {
-			ensure_installed = { "lua_ls", "ts_ls", "jsonls", "bashls" },
+			ensure_installed = { "lua_ls", "ts_ls", "jsonls", "bashls", "intelephense" },
 		},
 	},
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
-			vim.lsp.enable({ "lua_ls", "ts_ls", "jsonls", "bashls" })
+			vim.lsp.enable({ "lua_ls", "ts_ls", "jsonls", "bashls", "intelephense" })
 
 			vim.api.nvim_create_autocmd("LspAttach", {
 				group = vim.api.nvim_create_augroup("LspKeymaps", { clear = true }),
